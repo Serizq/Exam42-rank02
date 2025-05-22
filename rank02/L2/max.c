8ad81535d9c41b1a223b951060e86ff6ba846bc0
Sergio Izquierdo Alba <seizquie@c2r1s2.42madrid.com>
2025-05-22 15:19:53 +0200

    new

---
+++ rank02/L2/max.c
@@ -0,0 +1,33 @@
+/*Write the following function:
+
+int		max(int* tab, unsigned int len);
+
+The first parameter is an array of int, the second is the number of elements in
+the array.
+
+The function returns the largest number found in the array.
+
+If the array is empty, the function returns 0.******* */
+#include <stdio.h>
+
+int	max(int *tab, unsigned int len)
+{
+	if(len == 0)
+		return(0);
+	unsigned int	result;
+	int	i = 0;
+	while(tab[i])
+	{
+		if(i < len)
+			result = tab[i];
+		i++;
+	}
+	return(result);
+}
+int	main()
+{
+	int	array[] = {25, 398, 2};
+
+	printf("el número más grande es: %i\n", max(array, 3));
+	return(0);
+}
