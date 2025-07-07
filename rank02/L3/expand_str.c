65d98b63b3b8632b0a5b545a8479dc9c7fc1a178
Sergio <serizquierdoprof@gmail.com>
2025-07-07 21:17:01 +0200

    ft_list

--- rank02/L3/expand_str.c
+++ rank02/L3/expand_str.c
@@ -31,17 +31,19 @@ $>
 
 int	main(int argc, char **argv)
 {
-	char	*str = argv[1];
 	int	i = 0;
 	int	space = 0;
+	char	*str = argv[1];
 
 	if(argc == 2)
 	{
+		while(str[i] == ' ' || str[i] == '\t')
+			i++;
 		while(str[i] != '\0')
 		{
-			if(i > 0 && (str[i] == ' ' || str[i] == '\t'))
+			if(i >= 0 && (str[i] == ' ' || str[i] == '\t'))
 				space = 1;
-			if(i > 0 && str[i] != ' ' && str[i != '\t'] && str[i] != '\0')
+			if(i >= 0 && str[i] != ' ' && str[i] != '\t' && str[i] != '\0')
 			{
 				if(space)
 					write(1, "   ", 3);
@@ -53,4 +55,5 @@ int	main(int argc, char **argv)
 	}
 	write(1, "\n", 1);
 	return(0);
+
 }
